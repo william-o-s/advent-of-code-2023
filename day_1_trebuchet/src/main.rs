4cc96af1b0a482c5a4d06d1316d97e39ba84872c
William Setiawan <z5388080@ad.unsw.edu.au>
2024-07-31 15:27:41 +1000

    Added Day 1: Trebuchet

--- day_1_trebuchet/src/main.rs
+++ day_1_trebuchet/src/main.rs
@@ -1,3 +1,50 @@
+use std::fs;
+
+fn parse_using_loop(substring: &str) -> u32 {
+    let mut first_num: Option<char> = None;
+    let mut last_num: Option<char> = None;
+
+    for chr in substring.chars() {
+        if chr.is_numeric() {
+            if first_num.is_none() {
+                first_num = Some(chr);
+            }
+
+            last_num = Some(chr);
+        }
+    }
+
+    let first_num = first_num.unwrap().to_digit(10).unwrap();
+    let last_num = last_num.unwrap().to_digit(10).unwrap();
+
+    (first_num * 10) + last_num
+}
+
+fn parse_using_vec(substring: &str) -> Result<u32, String> {
+    let numbers = substring
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .collect::<Vec<u32>>();
+
+    if numbers.is_empty() {
+        return Err(format!("No numbers found in substring: {}", substring));
+    }
+
+    let first = numbers.first().unwrap_or(&0);
+    let last = numbers.last().unwrap_or(&0);
+    Ok((first * 10) + last)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filepath = "src/input.txt";
+
+    let contents = fs::read_to_string(filepath)
+        .expect("Please provide a valid input.txt file.");
+
+    let mut total = 0;
+    for substring in contents.lines() {
+        total += parse_using_vec(substring).unwrap_or(0);
+    }
+
+    println!("Total: {total}");
 }
